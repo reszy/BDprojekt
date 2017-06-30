ae18fc487fce754a5d0447f4f85448af8da7753a
Krzysztof Raszka (krzyras083) <krzyras083@student.polsl.pl>
2017-06-30 18:28:24 +0200

    labWorker labManager

--- BDprojekt/Laboratory/Worker/LabWorkerDialog.Designer.cs
+++ BDprojekt/Laboratory/Worker/LabWorkerDialog.Designer.cs
@@ -32,14 +32,18 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.saveButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // resultRichTextBox
             // 
             this.resultRichTextBox.Location = new System.Drawing.Point(6, 19);
             this.resultRichTextBox.Name = "resultRichTextBox";
-            this.resultRichTextBox.Size = new System.Drawing.Size(468, 249);
+            this.resultRichTextBox.ReadOnly = true;
+            this.resultRichTextBox.Size = new System.Drawing.Size(468, 46);
             this.resultRichTextBox.TabIndex = 0;
             this.resultRichTextBox.Text = "";
             // 
@@ -48,14 +52,14 @@
             this.groupBox1.Controls.Add(this.resultRichTextBox);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(480, 280);
+            this.groupBox1.Size = new System.Drawing.Size(480, 74);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Wynik badania";
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(336, 298);
+            this.saveButton.Location = new System.Drawing.Point(336, 247);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(75, 23);
             this.saveButton.TabIndex = 9;
@@ -64,24 +68,44 @@
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(417, 298);
+            this.cancelButton.Location = new System.Drawing.Point(417, 247);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 10;
             this.cancelButton.Text = "Anuluj";
             this.cancelButton.UseVisualStyleBackColor = true;
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 92);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(480, 149);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Wynik badania";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(468, 116);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
             // LabWorkerDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 332);
+            this.ClientSize = new System.Drawing.Size(504, 278);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.groupBox1);
             this.Name = "LabWorkerDialog";
             this.Text = "LabWorkerDialog";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -92,5 +116,7 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
